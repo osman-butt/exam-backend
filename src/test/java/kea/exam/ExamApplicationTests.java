482f41e7dfef36094373f172a85671ef75fc3264
osman-butt <butt.osman@gmail.com>
2024-06-20 10:10:11 +0200

    initial commit

---
+++ src/test/java/kea/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
