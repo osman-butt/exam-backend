1444d785c32f0133e427bbdcc240e2162fd2dbab
osman-butt <butt.osman@gmail.com>
2024-06-20 10:12:58 +0200

    Create HomeController.java

---
+++ src/main/java/kea/exam/controller/HomeController.java
@@ -0,0 +1,19 @@
+package kea.exam.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    @GetMapping
+    public ResponseEntity<Home> home() {
+        return ResponseEntity.ok(new Home("API is running!"));
+    }
+
+    public record Home(String message) {
+    }
+}
